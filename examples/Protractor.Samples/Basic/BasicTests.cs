202dc1eeec8668c2be48795e1492ddbd86a1fa83
Bao Trinh <git@baodtrinh.com>
2018-04-19 13:26:04 -0500

    Add path to build directory so selenium can find downloaded chromedriver

--- examples/Protractor.Samples/Basic/BasicTests.cs
+++ examples/Protractor.Samples/Basic/BasicTests.cs
@@ -21,7 +21,7 @@ namespace Protractor.Samples.Basic
             //driver = new PhantomJSDriver();
 
             // Using Chrome
-            driver = new ChromeDriver();
+            driver = new ChromeDriver("./");
 
             // Using Internet Explorer
             //var options = new InternetExplorerOptions() { IntroduceInstabilityByIgnoringProtectedModeSettings = true };
